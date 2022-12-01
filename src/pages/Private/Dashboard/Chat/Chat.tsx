a8118bfda50ccebf22d9b829a3e5e77193f9fb56
david hernandez <davidfelipehernandez1@gmail.com>
2022-12-01 17:29:21 -0500

    chat comp and contacts state udpate

--- src/pages/Private/Dashboard/Chat/Chat.tsx
+++ src/pages/Private/Dashboard/Chat/Chat.tsx
@@ -1,4 +1,32 @@
+import { UserInfo } from '@/models';
+import { contactsKey } from '@/redux/state/contacts';
+import { useParams } from 'react-router-dom';
+import ChatNav from './ChatNav/ChatNav';
+import ChatWrapper from './ChatWrapper.css';
+import { MdKeyboardArrowRight } from 'react-icons/md';
+import React from 'react';
+
+const handleSubmit = (e: React.FormEvent) => {
+  e.preventDefault();
+};
+
 function Chat() {
-  return <div>Chat</div>;
+  const contacts = JSON.parse(localStorage.getItem(contactsKey) as string);
+  const { contactId } = useParams();
+  const contact = contacts?.res.find((item: UserInfo) => String(item.id) === contactId);
+
+  return (
+    <ChatWrapper>
+      <ChatNav contact={contact} />
+      <article className="chat" onSubmit={handleSubmit}>
+        <form action="">
+          <input type="text" className="chat__input" placeholder="enter a message..." />
+          <button className="chat__btn" type="submit">
+            <MdKeyboardArrowRight className="chat__btn__icon" />
+          </button>
+        </form>
+      </article>
+    </ChatWrapper>
+  );
 }
 export default Chat;
--- src/redux/state/contacts.ts
+++ src/redux/state/contacts.ts
@@ -1,11 +1,8 @@
 import { ContactInfo } from '@/models';
+import { addLocalStorage, removeLocalStorage } from '@/utilities';
 import { createSlice } from '@reduxjs/toolkit';
 
-const EmptyInitialContactState: ContactInfo = {
-  id: 0,
-  name: '',
-  image: '',
-};
+const EmptyInitialContactState: ContactInfo[] = [];
 
 export const contactsKey = 'contacts';
 
@@ -13,12 +10,22 @@ export const contactsSlice = createSlice({
   name: 'contacts',
   initialState: EmptyInitialContactState,
   reducers: {
-    createContacts: (state, action) => action.payload,
-    updateContacs: (state, action) => ({ ...state, ...action.payload }),
-    resetContacts: (state, action) => EmptyInitialContactState,
+    createContacts: (state, action) => {
+      addLocalStorage(contactsKey, { res: [...action.payload] });
+      return action.payload;
+    },
+    updateContacs: (state, action) => {
+      const result = [...state, ...action.payload];
+      addLocalStorage(contactsKey, result);
+      return result;
+    },
+    removeContacts: () => {
+      removeLocalStorage(contactsKey);
+      return EmptyInitialContactState;
+    },
   },
 });
 
-export const { createContacts, updateContacs, resetContacts } = contactsSlice.actions;
+export const { createContacts, updateContacs, removeContacts } = contactsSlice.actions;
 
 export default contactsSlice.reducer;
